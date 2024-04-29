52e4f96f1ab21eac345bde4c2ddcc7d806a5de61
Cyrus Moosavinia <cyrusmoosavinia@gmail.com>
2024-04-29 23:53:17 +0100

    created information card component

---
+++ src/app/information-card/information-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-information-card',
+  standalone: true,
+  imports: [],
+  template: ` <p>information-card works!</p> `,
+  styleUrl: './information-card.component.css',
+})
+export class InformationCardComponent {}
